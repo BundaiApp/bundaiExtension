2e869721113d5e582ff8fc13b7b3845827954c71
msazzad-42161 <mdsazzadul.islam.42161@gmail.com>
2025-09-25 18:11:15 +0600

    using a ssh key for bundai secure key

--- utils/secure-storage.ts
+++ utils/secure-storage.ts
@@ -1,4 +1,4 @@
 import { SecureStorage } from "@plasmohq/storage/secure"
 
 export const storage = new SecureStorage()
-export const storageReady = storage.setPassword("bundai-secure-key")
+export const storageReady = storage.setPassword(process.env.PLASMO_SECURE_STORAGE_PASSWORD)
